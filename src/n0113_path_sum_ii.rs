87bbc0482e36335b2ad6bba5687f50bf56cb64de
frezc <504021398@qq.com>
2019-06-28 00:04:43 +0800

    113

--- src/n0113_path_sum_ii.rs
+++ src/n0113_path_sum_ii.rs
@@ -56,6 +56,20 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> Vec<Vec<i32>> {
+        if let Some(node) = root {
+            if node.borrow().val == sum {
+                if node.borrow().left.is_none() && node.borrow().right.is_none() {
+                    return vec![vec![node.borrow().val]];
+                }
+            }
+
+            let val = node.borrow().val;
+            let mut result = [Solution::path_sum(node.borrow().left.clone(), sum - val),
+                Solution::path_sum(node.borrow().right.clone(), sum - val)].concat();
+            result.iter_mut().for_each(|path| path.insert(0, val));
+            return result;
+        }
+
         vec![]
     }
 }
@@ -69,5 +83,11 @@ mod tests {
 
     #[test]
     fn test_113() {
+        assert_eq!(Solution::path_sum(tree![5,4,8,11,n,13,4,7,2,n,n,5,1], 22), vec2![
+           [5,4,11,2],
+           [5,8,4,5]
+        ]);
+        assert_eq!(Solution::path_sum(tree![], 0), vec![vec![0i32;0];0]);
+        assert_eq!(Solution::path_sum(tree![1,-2,-3,1,3,-2,null,-1], -1), vec2![[1,-2,1,-1]]);
     }
 }
