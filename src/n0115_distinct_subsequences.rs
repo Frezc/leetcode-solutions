f7b1fd7ab1beade1633ed1c8c36aefa17d9c0d33
frezc <frezcw@gmail.com>
2019-07-04 23:52:48 +0800

    #115 comment

--- src/n0115_distinct_subsequences.rs
+++ src/n0115_distinct_subsequences.rs
@@ -46,6 +46,18 @@
  * 
  * 
  */
+/// A simple DP question
+/// Iterate solution:
+/// Suppose i, j are end indices of substring of t and s, like s.substring(0, j+1) & t.substring(0, i+1)
+/// Allocate dp with t.len * s.len, and fill 0 to it.
+/// The key recursion is if s[j] == t[i], dp[i][j] = dp[i-1][j-1] + dp[i][j-1] else dp[i][j] = dp[i][j-1].
+///
+/// For example, suppose s = "babgbag", t = "bag", j = 3, i = 2.
+/// Because s[j] == t[i], we can convert num_distinct("babg", "bag") to num_distinct("bab", "ba") + num_distinct("bab", "bag").
+/// The first is matching current char and the second is not matching current char (just drop it).
+///
+/// Recursive solution:
+/// Same
 pub struct Solution {}
 
 // submission codes start here
@@ -95,7 +107,8 @@ mod tests {
     fn test_115() {
         assert_eq!(Solution::num_distinct("rabbbit".to_string(), "rabbit".to_string()), 3);
         assert_eq!(Solution::num_distinct("babgbag".to_string(), "bag".to_string()), 5);
-        assert_eq!(Solution::num_distinct("".to_string(), "bag".to_string()), 0);
+        assert_eq!(Solution::num_distinct("babgbag".to_string(), "bag".to_string()), 5);
+        assert_eq!(Solution::num_distinct("bababa".to_string(), "bag".to_string()), 0);
         assert_eq!(Solution::num_distinct("".to_string(), "".to_string()), 0);
         assert_eq!(Solution::num_distinct("a".to_string(), "".to_string()), 0);
     }
