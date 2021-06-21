10407166c5ac228124aa6b12eae5e3d978d2ab99
frezc <frezcw@gmail.com>
2021-06-21 23:23:15 +0800

    yanghuisanjiao

---
+++ leetcode-challenge/2021-06/Pascal's Triangle.ts
@@ -0,0 +1,12 @@
+function generate(numRows: number): number[][] {
+    const result = [[1]];
+    for (let i = 1; i < numRows; i++) {
+        const row = [1];
+        for (let j = 1; j < i; j++) {
+            row.push(result[i - 1][j - 1] + result[i - 1][j]);
+        }
+        row.push(1);
+        result.push(row);
+    }
+    return result;
+};
